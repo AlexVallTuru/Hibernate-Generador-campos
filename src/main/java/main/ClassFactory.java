911a49c2e641478d8a50954f044bc694562ed96e
vall.turu.alex <vall.turu.alex@alumnat.copernic.cat>
2023-03-01 23:57:43 +0100

    Test RF04 --> Done -Creacion classe Armas(Funciones para rellenar) -Modificación clase Mecanic, Pilot, Soldat

--- src/main/java/main/ClassFactory.java
+++ src/main/java/main/ClassFactory.java
@@ -5,6 +5,7 @@
 package main;
 
 import Interficies.TesteableFactory;
+import Utils.Armas;
 import com.github.javafaker.Faker;
 import entitats.Aeronau;
 import entitats.Combat;
@@ -134,11 +135,33 @@ public class ClassFactory implements TesteableFactory {
         Soldat soldat = null;
 
         if (tipus == Mecanic.class) {
-            soldat = new Mecanic();
+            soldat = new Mecanic(
+                    fake.number().randomDigit(),
+                    null,
+                    (float)fake.number().randomDouble(2, 0, 100),
+                    fake.number().numberBetween(0, 150),
+                    fake.number().randomDigit(),
+                    fake.number().randomDigit(),
+                    Armas.generadorArmasPrincipales(fake.number().numberBetween(0, 99)),
+                    Armas.generadorArmasSecundarias(fake.number().numberBetween(0, 99)),
+                    Armas.generadorPrestigios(fake.number().numberBetween(0, 9)),
+                    Armas.generadorArmaCQC(fake.number().numberBetween(0, 99)),
+                    fake.bool().bool(),
+                    Utils.localDateToSQLDate(fake.date().birthday()));
 
         } else if (tipus == Pilot.class) {
-            soldat = new Pilot();
-        }
+            soldat = new Pilot(fake.aviation().aircraft(),
+                    null,
+                    (float)fake.number().randomDouble(2, 0, 100),
+                    fake.number().numberBetween(0, 150),
+                    fake.number().randomDigit(),
+                    fake.number().randomDigit(),
+                    Armas.generadorArmasPrincipales(fake.number().numberBetween(0, 99)),
+                    Armas.generadorArmasSecundarias(fake.number().numberBetween(0, 99)),
+                    Armas.generadorPrestigios(fake.number().numberBetween(0, 9)),
+                    Armas.generadorArmaCQC(fake.number().numberBetween(0, 99)),
+                    fake.bool().bool(),
+                    Utils.localDateToSQLDate(fake.date().birthday()));        }
         return soldat;
     }
 
